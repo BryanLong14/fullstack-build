b3037f2671fd408378ee10729647ce9474ee0748
Bryan Long <BryanLong1@gmail.com>
2018-03-16 12:23:30 -0600

    Clientside React Boilerplate Code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className="App">
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">
+              Barkwire
+            </a>
+          </nav>
+        </header>
+        <main className="container">
+        <h2>
+        Dogs! 
+        </h2>
+        </main>
+      </div>;
+  }
+}
+
+export default App;
